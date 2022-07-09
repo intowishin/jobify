8349b38c9a7c5ff43dff7aed413ed225e0aae265
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-07-08 22:42:36 -0400

    landing page - complete

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,14 +1,15 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import styled from "styled-components";
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="jobify" className="logo" />
       </nav>
       <div className="container page">
-      {/* info */}
+        {/* info */}
         <div className="info">
           <h1>
             job <span>tracking</span> app
@@ -19,11 +20,49 @@ const Landing = () => {
             thundercats lo-fi put a bird on it butcher venmo bespoke. Unicorn
             synth brooklyn cardigan cronut.
           </p>
-            <button className="btn btn-hero">Login/Register</button>
+          <button className="btn btn-hero">Login/Register</button>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
-    </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: clac(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1{
+    font-size: 700;
+    span{
+        color: var(--primary-500);
+    }
+  }
+  p{
+    color: var(--grey-600);
+  }
+  .main-img{
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page{
+        grid-template-columns: 1fr 1fr;
+        colimn-gap: 3rem;
+    }
+    .main-img{
+        display: block;
+    }
+  }
+`;
 export default Landing;
