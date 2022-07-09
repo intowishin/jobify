e065dc128bd91b334719bd9dcd8755dfea0da578
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-07-08 22:18:58 -0400

    landing page - structure

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,29 @@
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
-
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
 
 const Landing = () => {
   return (
-<h4>Landing Page</h4>  )
-}
-export default Landing
+    <main>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+      {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby mixtape tilde raw denim biodiesel iceland letterpress.
+            Umami vinyl pug, direct trade seitan truffaut retro cold-pressed
+            thundercats lo-fi put a bird on it butcher venmo bespoke. Unicorn
+            synth brooklyn cardigan cronut.
+          </p>
+            <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
+};
+export default Landing;
