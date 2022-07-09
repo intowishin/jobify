1ebd78b90645c3c9a90ca3ebaa3750f53bb7eee9
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-07-08 23:23:07 -0400

    Link Component

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
 import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -20,7 +21,7 @@ const Landing = () => {
             thundercats lo-fi put a bird on it butcher venmo bespoke. Unicorn
             synth brooklyn cardigan cronut.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to='/register' className="btn btn-hero">Login/Register</Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
